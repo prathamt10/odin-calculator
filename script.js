3de1e4a073e8460d7fd51a218988092b836a3840
prathamt10 <prathamtalware11@gmail.com>
2024-01-28 15:10:30 +0530

    calculator backend complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let num1 = '';
 let num2 = '';
 let operator = '';
+let isNumFloat = false;
 
 const cur_number_screen = document.querySelector('.cur-number');
 const last_number_screen = document.querySelector('.last-number');
@@ -28,37 +29,38 @@ function divide() {
 }
 
 function operate() {
-    if (num1 != '' && num2 == '') {
+    if ((num1 != '' && num2 == '') || (num1 == '' && num2 == '')) {
         return num1;
     }
     let res = '';
-    if (num1 != '' && num2 != '') {
-        switch (operator) {
-            case '+':
-                res = add();
-                break;
-                
-            case '-':
-                res = subtract();
-                break;
+    switch (operator) {
+        case '+':
+            res = add();
+            break;
+            
+        case '-':
+            res = subtract();
+            break;
 
-            case 'x':
-                res = multiply();
-                break;
-                
-            case '/':
-                res = divide();
-                break;
-        
-            default:
-                break;
-        }
+        case 'x':
+            res = multiply();
+            break;
+            
+        case '/':
+            res = divide();
+            break;
+    
+        default:
+            break;
     }
     return res;
     
 }
 
 function addNumber(num) {
+    if (num == '.' && isNumFloat) {
+        return;
+    }
     if (operator == '') {
         num1 += num;
         cur_number_screen.textContent = num1;
@@ -67,22 +69,34 @@ function addNumber(num) {
         last_number_screen.textContent = `${num1} ${operator}`;
         cur_number_screen.textContent = num2;
     }
+    if (num == '.') {
+        isNumFloat = true;
+    }
 }
 
 operator_buttons.forEach((operator_button) => {
     operator_button.addEventListener('click', (e) => {
         operator = e.target.textContent;
         last_number_screen.textContent = `${num1} ${operator}`;
+        isNumFloat = false;
     });
 });
 
 equals_button.addEventListener('click', () => {
+    if (parseFloat(num2) == 0) {
+        alert("C'mon Man");
+        return;
+    }
     last_number_screen.textContent = `${num1} ${operator} ${num2} =`;
     const res = operate();
     cur_number_screen.textContent = res;
     num1 = res;
     num2 = '';
     operator = '';
+    isNumFloat = false;
+    if (!Number.isInteger(parseFloat(num1))) {
+        isNumFloat = true;
+    }
 });
 
 number_buttons.forEach((number_button) => {
@@ -91,17 +105,25 @@ number_buttons.forEach((number_button) => {
     });
 });
 
+decimal_button.addEventListener('click', (e) => {
+    addNumber(e.target.textContent);
+})
+
 clear_button.addEventListener('click', () => {
     cur_number_screen.textContent = '';
     last_number_screen.textContent = '';
     num1 = '';
     num2 = '';
     operator = '';
+    isNumFloat = false;
 });
 
 delete_button.addEventListener('click', () => {
+    if (cur_number_screen.textContent[-1] == '.') {
+        isNumFloat = false;
+    }
     cur_number_screen.textContent = cur_number_screen.textContent.slice(0, -1);
-    if (num2 == '') {
+    if (operator == '') {
         num1 = cur_number_screen.textContent;
     } else {
         num2 = cur_number_screen.textContent;
